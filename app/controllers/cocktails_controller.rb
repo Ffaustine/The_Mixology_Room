3eec05be22a584f730a6e6a020de2f4fa86796a8
Faustine Fourcade <faustine.fourcade@hotmail.fr>
2023-08-03 18:24:46 +0200

    viex search results

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,6 +6,7 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
     if params[:query].present?
+      session[:search_results] ||= []
       search
     end
   end
@@ -14,7 +15,6 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
     @cocktail_ingredients = @cocktail.cocktail_ingredients
   end
-
 
   private
 
@@ -27,31 +27,42 @@ class CocktailsController < ApplicationController
     @cocktails = []
     if results.present?
       results.each do |result|
-        if cocktail == Cocktail.find_by(name: result["strDrink"])
+        existing_cocktail_id = session[:search_results].find { |id| id == result["idDrink"] }
+        if existing_cocktail_id
+          cocktail = Cocktail.find(existing_cocktail_id)
           @cocktails << cocktail
         else
-          cocktail = Cocktail.create!(
-            name: result["strDrink"],
-            image_url: result["strDrinkThumb"],
-            category: result["strCategory"],
-            alcoholic: result["strAlcoholic"],
-            IBA: result["strIBA"],
-            glass: result["strGlass"],
-            instructions: result["strInstructions"]
-          )
-          1.upto(15) do |i|
-            ingredient_name = result["strIngredient#{i}"]
-            if ingredient_name.present?
-              ingredient = Ingredient.find_or_create_by(name: ingredient_name)
-              amount_oz = result["strMeasure#{i}"].to_f
-              amount_cl = amount_oz * 2.957
+          cocktail = Cocktail.find_by(name: result["strDrink"])
+          unless cocktail
+            cocktail = Cocktail.create!(
+              name: result["strDrink"],
+              image_url: result["strDrinkThumb"],
+              category: result["strCategory"],
+              alcoholic: result["strAlcoholic"],
+              IBA: result["strIBA"],
+              glass: result["strGlass"],
+              instructions: result["strInstructions"]
+            )
+            1.upto(15) do |i|
+              ingredient_name = result["strIngredient#{i}"]
+              if ingredient_name.present?
+                ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+                amount_oz = result["strMeasure#{i}"].to_f
+                amount_cl = amount_oz * 2.957
 
-              CocktailIngredient.create!(cocktail: cocktail, ingredient: ingredient, amount: amount_cl.round)
+                CocktailIngredient.create!(cocktail: cocktail, ingredient: ingredient, amount: amount_cl.round)
+              end
             end
           end
           @cocktails << cocktail
+          session[:search_results] << cocktail.id
         end
       end
     end
+    render "search_results"
+  end
+
+  def search_results
+    @cocktails = session[:search_results] || []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "cocktails#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show]
+  get 'cocktails/search_results', to: 'cocktails#search_results', as: 'search_results'
 end
